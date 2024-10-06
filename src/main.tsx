a187ebc1349e512cd7f0a665393c45dc54c6b6a9
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2024-10-06 23:14:44 +0300

    feat: add styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/700.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import { App } from "./app.tsx";
+import "@/app/styles/index.scss";
+
+import { App } from "./app/app";
 
 createRoot(document.getElementById("root")! as HTMLElement).render(
   <StrictMode>
