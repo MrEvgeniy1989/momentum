0f702a4f37b302717cb99bc49e56204ebbc36eb9
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2024-10-06 21:58:42 +0300

    Initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,23 @@
+import antfu from "@antfu/eslint-config";
+
+export default antfu({
+  type: "app",
+  react: true,
+  typescript: true,
+  formatters: true,
+  stylistic: {
+    semi: true,
+    quotes: "double",
+  },
+}, {
+  rules: {
+    "no-console": ["warn"],
+    "perfectionist/sort-imports": ["error", {
+      internalPattern: ["@/**"],
+    }],
+    "unicorn/filename-case": ["error", {
+      case: "kebabCase",
+      ignore: ["README.md"],
+    }],
+  },
+});
