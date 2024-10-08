bd90983154d8cb0c60f61425fa24dd7d46b1b3d3
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2024-10-08 13:44:54 +0300

    feat: Added seconds to the clock and date.

--- src/entities/time/ui/time.tsx
+++ src/entities/time/ui/time.tsx
@@ -8,19 +8,38 @@ export function Time({ time }: Props) {
   const formatTime = (date: Date): string[] => {
     const hours = date.getHours().toString().padStart(2, "0");
     const minutes = date.getMinutes().toString().padStart(2, "0");
-    return [hours, minutes];
+    const seconds = date.getSeconds().toString().padStart(2, "0");
+    return [hours, minutes, seconds];
   };
 
-  const [hours, minutes] = formatTime(time);
+  const formatDate = (date: Date): string => {
+    const day = date.toLocaleDateString("ru-RU", { day: "2-digit" });
+    const month = date.toLocaleDateString("ru-RU", { day: "2-digit", month: "long" }).split(" ")[1];
+    const weekday = date.toLocaleDateString("ru-RU", { weekday: "long" });
+    return `${day} ${month}, ${weekday}`;
+  };
+
+  const [hours, minutes, seconds] = formatTime(time);
+  const formattedDate = formatDate(time);
 
   return (
-    <div className={s.time}>
-      <div className={s.timeBlock}>{hours}</div>
-      <div className={s.dots}>
-        <span className={s.dot}></span>
-        <span className={s.dot}></span>
+    <div className={s.clockAndDate}>
+      <div className={s.time}>
+        <div className={s.timeBlock}>{hours}</div>
+        <div className={s.dots}>
+          <span className={s.dot}></span>
+          <span className={s.dot}></span>
+        </div>
+        <div className={s.timeBlock}>{minutes}</div>
+        <div className={s.dots}>
+          <span className={s.dot}></span>
+          <span className={s.dot}></span>
+        </div>
+        <div className={s.timeBlock}>{seconds}</div>
       </div>
-      <div className={s.timeBlock}>{minutes}</div>
+      <div>
+        <div className={s.date}>{formattedDate}</div>
+      </div>
     </div>
   );
 }
