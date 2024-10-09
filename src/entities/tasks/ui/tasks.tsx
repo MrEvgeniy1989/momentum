aaa3c4e6e1446808a8b97e7ac21f247a90443261
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2024-10-09 22:11:55 +0300

    feat: Added layout for empty Tasks.

--- src/entities/tasks/ui/tasks.tsx
+++ src/entities/tasks/ui/tasks.tsx
@@ -1,7 +1,55 @@
+import { useState } from "react";
+
+import { CloseIcon } from "@/assets/icons/close-icon";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from "@/common/components/ui/dropdown";
+import s from "@/entities/tasks/ui/tasks.module.scss";
+
 export function Tasks() {
+  const [showInput, setShowInput] = useState(false);
+
   return (
-    <div>
-      Tasks
-    </div>
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <button type="button" className={s.buttonTrigger}>Tasks</button>
+      </DropdownMenuTrigger>
+
+      <DropdownMenuContent className={s.dropdownContent}>
+        <div className={s.mainContentWithoutTasks}>
+          <div className={s.mainContentWithoutTasksHeader}>
+            <span>Today</span>
+          </div>
+          <div className={s.mainContentWithoutTasksBody}>
+            <span>Add a task to get started</span>
+            {!showInput && (
+              <button
+                type="button"
+                className={s.mainContentWithoutTasksBodyButton}
+                onClick={() => setShowInput(true)}
+              >
+                New Task
+              </button>
+            )}
+          </div>
+          <div className={s.mainContentWithoutTasksFooter}>
+            {showInput && (
+              <div className={s.inputBox}>
+                <input type="text" className={s.input} placeholder="New Task" autoFocus />
+                <button
+                  className={s.buttonCloseIcon}
+                  onClick={() => { setShowInput(false); }}
+                  type="button"
+                >
+                  <CloseIcon />
+                </button>
+              </div>
+            )}
+          </div>
+        </div>
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 }
